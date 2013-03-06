ca340e17cc4d7ef0d091391056ae92bd9f049b7a
Richard Hind <themallrat1@gmail.com>
2013-03-06 10:43:29 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Formula::Application.config.secret_token = '95514d28a7b9235063865941a3b3574428b0b177f1ea18f1e21182501ef83f528be6c86399e83a5303b7a17cef1afe93234b49b636a9cd1744e4c68655654192'
